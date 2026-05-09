c980b960ee7a388f53b8e2d3547d9389e9732d75
Pravin Rajpurohit <pravinrajpurohit479@gmail.com>
2026-05-09 16:52:53 +0530

    feat: Build currency converter using custom hook

---
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import { use } from 'react';
+import {useEffect, useState} from 'react';
+
+function useCurrencyInfo(currency){                  //whenever creating custom hooks naming start with 'use...'                                                                               ex.)  useCurrencyInfo(), useCounter 
+
+        const [data, setData] = useState({})
+        useEffect(()=> {
+            fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+            .then((res)=> res.json())
+            .then((res)=> setData(res[currency]) )
+            
+            
+        }, [currency])     
+        
+       
+        
+        
+        return data
+}   
+
+export default useCurrencyInfo;
